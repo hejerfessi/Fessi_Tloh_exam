5f8646c148c64bdf332586c4842772df48e3e483
hejerfessi <hejer.fessi@gmail.com>
2022-12-06 23:44:01 +0100

    adding hashData service

--- src/main/scala/service1/GDPRservice.scala
+++ src/main/scala/service1/GDPRservice.scala
@@ -3,6 +3,7 @@ package service1
 import config.Client
 import config.Config.{Config, argParser}
 import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.catalyst.expressions.XxHash64
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.{Dataset, SparkSession}
 import scopt.OParser
@@ -21,42 +22,47 @@ object DeletePerson {
 
     val spark = SparkSession
       .builder
-      .appName("SparkSQL")
-      .master("local[*]")
+      .appName("SparkSQ")
       .getOrCreate()
 
     OParser.parse(argParser, args, Config()) match {
       case Some(config) =>
         println(OParser.usage(argParser))
         // do stuff with config
-
+        if (config.service == "deleteService"){
         println(config.idClient)
         println(config.filepath)
         println(config.finalpath)
+
         readDataset(spark,config.filepath)
-        deleteClient(config.idClient,spark,config.filepath,config.finalpath)
+        deleteClient(config.idClient,spark,config.filepath)
+        }else {
+
+
+        }
       case _ =>
         exit(1)
     }
-
 
     spark.stop()
   }
 
   // Load each line of the source data into an Dataset
-  def readDataset(sparkSession: SparkSession,path:String):Dataset[Client]= {
+  def readDataset(sparkSession: SparkSession ,path:String):Dataset[Client]= {
     import sparkSession.implicits._
     val df = sparkSession.read
       .option("header", "true")
-      .option("inferSchema", "true")
+      .option("delimiter", ";")
       .csv(path)
+      .coalesce(1)
       .as[Client]
+
     df
+
   }
 
-
   def delete(dataset: Dataset[Client],id:Long):Dataset[Client] ={
-    val data_filtred = dataset.filter (! col ("identifiantClient").isin (id) )
+    val data_filtred = dataset.filter (! col ("identifiantClient").isin(id) )
     data_filtred.show()
     data_filtred
   }
@@ -66,16 +72,35 @@ object DeletePerson {
       .option("header", true)
       .option("delimiter",";")
       .mode("overwrite")
-      .csv(path)
+      .csv("/out")
+
+//    import scala.sys.process._
+//
+//    s"hdfs dfs -rm /fessi_tloh_tp/bronze/data_client.csv".!
+//    s"hdfs dfs -mv /fessi_tloh_tp/bronze/data_client2.csv fessi_tloh_tp/bronze/data_client.csv".!
+//    dataset.show(10,false)
 
   }
 
-  def deleteClient(id:Int,sparkSession: SparkSession,path:String,finalpath:String): Unit ={
+  def deleteClient(id:Int,sparkSession: SparkSession,path:String): Unit ={
     val dataset1 = readDataset(sparkSession,path)
     val dataset_filtred= delete(dataset1,id)
     dataset_filtred.show()
-    writeDataset(dataset_filtred,finalpath)
+    writeDataset(dataset_filtred,path)
 
   }
 
+  def hash(s:String): String ={
+    val hash = XxHash64.hashCode(s)
+  }
+
+  def hashData(sparkSession: SparkSession,dataset: Dataset[Client],id:Int): Unit ={
+
+    val data_filtred = dataset.filter (! col ("identifiantClient").isin(id) )
+    val data_filtred2 = dataset.filter (col ("identifiantClient").isin(id) ).map(client => (client.identifiantClient,client.nom.))
+
+
+
+
+  }
 }
